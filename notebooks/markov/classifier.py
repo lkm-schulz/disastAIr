faae304bd88c6b7d078b8b4a4178f5015df76df5
Lennart Schulz <lkm.schulz@gmail.com>
2024-03-26 17:31:37 +0100

    feat: use instance + label lists for classifier

--- notebooks/markov/classifier.py
+++ notebooks/markov/classifier.py
@@ -11,33 +11,43 @@ class Classifier:
     __log_prior_neg__: float
     order: int
 
-    def __init__(self, order: int, positives: list, negatives: list, verbose: bool = False, 
+    def __init__(self, order: int, instances: list, labels: list, multithread:bool = False, verbose: bool = False, 
                  vocab_size: int = DEF_VOCAB_SIZE, smoothing_weight: float = DEF_SMOOTHING_WEIGHT):
         
-        self.model_pos = Model(order, positives, multithread=True, verbose=verbose, vocab_size=vocab_size, smoothing_weight=smoothing_weight)
-        self.model_neg = Model(order, negatives, multithread=True, verbose=verbose, vocab_size=vocab_size, smoothing_weight=smoothing_weight)
-        self.__log_prior_pos__ = math.log(len(positives)) - math.log((len(positives) + len(negatives)))
-        self.__log_prior_neg__ = math.log(len(negatives)) - math.log((len(positives) + len(negatives)))
+        positives = []
+        negatives = []
+
+        if (len(instances) != len(labels)):
+            raise ValueError('Instances and labels must have the same length.')
+        
+        for i in range(len(instances)):
+            if labels[i]:
+                positives.append(instances[i])
+            else:
+                negatives.append(instances[i])
+
+        self.model_pos = Model(order, positives, multithread=multithread, verbose=verbose, vocab_size=vocab_size, smoothing_weight=smoothing_weight)
+        self.model_neg = Model(order, negatives, multithread=multithread, verbose=verbose, vocab_size=vocab_size, smoothing_weight=smoothing_weight)
         self.order = order
 
     def classify(self, seq) -> bool:
         return self.model_pos.get_log_sum_prob(seq) + self.__log_prior_neg__ > self.model_neg.get_log_sum_prob(seq) + self.__log_prior_neg__
 
-    def eval(self, positives: list, negatives: list) -> ConfusionMatrix:
+    def eval(self, instances: list, labels: list) -> ConfusionMatrix:
 
         res = ConfusionMatrix()
-
-        for instance in positives:
-            if self.classify(instance):
-                res.tp += 1
+        
+        for i in range(len(instances)):
+            if self.classify(instances[i]):
+                if labels[i]:
+                    res.tp += 1
+                else:
+                    res.fp += 1
             else:
-                res.fn += 1
-
-        for instance in negatives:
-            if not self.classify(instance):
-                res.tn += 1
-            else:
-                res.fp += 1
+                if labels[i]:
+                    res.fn += 1
+                else:
+                    res.tn += 1
         
         return res
     
